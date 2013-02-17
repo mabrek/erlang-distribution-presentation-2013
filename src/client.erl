095c2c13efbed76cd11ce3168b7d2cab40b26eda
Anton Lebedevich <mabrek@gmail.com>
2013-02-17 11:59:19 +0400

    simple client/server example

---
+++ src/client.erl
@@ -0,0 +1,9 @@
+-module(client).
+
+-export([start/0]).
+
+start() ->
+    Reply = gen_server:call({server_quux, 'node_foo@127.0.0.1'}, "hello"),
+    io:format("reply is ~p~n", [Reply]),
+    timer:sleep(30000),
+    erlang:halt().
---
+++ src/server.erl
@@ -0,0 +1,33 @@
+-module(server).
+
+-behaviour(gen_server).
+
+%% API
+-export([start/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {}).
+
+start() ->
+    gen_server:start_link({local, server_quux}, ?MODULE, [], []).
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_call("hello", _From, State) ->
+    {reply, "world", State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.

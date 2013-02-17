cb8ca5cfacf6d8bbf3a78ecb1c80b040068268ee
Anton Lebedevich <mabrek@gmail.com>
2013-02-17 22:52:07 +0400

    less comments in code

--- src/server.erl
+++ src/server.erl
@@ -2,10 +2,8 @@
 
 -behaviour(gen_server).
 
-%% API
 -export([start/0]).
 
-%% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 

a04d333e23e4ae178f008c8e0f331fb103c21427
Anton Lebedevich <mabrek@gmail.com>
2013-02-17 23:02:16 +0400

    formatting

--- src/client.erl
+++ src/client.erl
@@ -3,7 +3,9 @@
 -export([start/0]).
 
 start() ->
-    Reply = gen_server:call({server_quux, 'server_node@127.0.0.1'}, "hello"),
+    Reply = gen_server:call(
+              {server_quux, 'server_node@127.0.0.1'},
+              "hello"),
     io:format("reply is ~p~n", [Reply]),
     timer:sleep(30000),
     erlang:halt().
